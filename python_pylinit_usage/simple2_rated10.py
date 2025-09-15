41658dc34d7cd0bb9389c88a2f6d8a8a408e5f5b
Sahil Khan <sahkhan@microsoft.com>
2025-09-15 18:37:28 +0000

    Explored pylint in Python

---
+++ python_pylinit_usage/simple2_rated10.py
@@ -0,0 +1,13 @@
+"""
+A very simple script to demonstrate function definition and printing
+two variables.
+"""
+
+def myfunc():
+    """Print two numbers: first and second."""
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
